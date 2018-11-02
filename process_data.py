a26bd2405f534d2900898bcc001ed08877794a31
Kaitlin Naughten <kaight@bas.ac.uk>
2018-11-02 13:21:02 +0000

    Working on process_data

--- process_data.py
+++ process_data.py
@@ -8,19 +8,19 @@ from scipy.io import savemat
 from MITgcmutils import rdmds
 
 from mitgcm_python.utils import convert_ismr, z_to_xyz
-from mitgcm_python.make_domain import model_bdry, level_vars
-from mitgcm_python.file_io import read_binary
+from mitgcm_python.make_domain import model_bdry, level_vars, do_digging, do_zapping
+from mitgcm_python.file_io import read_binary, write_binary
 from mitgcm_python.interpolation import discard_and_fill
 
 
 # Put MITgcm melt rates in the right format for Ua. No need to interpolate.
 
 # Arguments:
-# grid: Grid object (for the MITgcm segment that just finished)
 # ismr_head: beginning of file paths of the binary files containing SHIfwFlx data, including the directory. i.e. ismr_head.xxxxx.data and ismr_head.xxxxx.meta where xxxxx is the diagnostic timestep.
 # ua_out_file: desired path to .mat file for Ua to read melt rates from.
+# grid: Grid object (for the MITgcm segment that just finished)
 
-def extract_melt_rates (grid, ismr_head, ua_out_file):
+def extract_melt_rates (ismr_head, ua_out_file, grid):
 
     # Read the most recent file containing ice shelf melt rate
     data, its, meta = rdmds(ismr_head, itrs=np.Inf, returnmeta=True)
@@ -40,16 +40,49 @@ def extract_melt_rates (grid, ismr_head, ua_out_file):
     savemat(ua_out_file, {'meltrate':ismr_points, 'x':lon_points, 'y':lat_points})  
 
 
-# TODO
-# 3 options here: no digging, dig bathymetry, dig ice shelf draft
-# Modify do_digging in mitgcm_python.make_domain to have a keyword argument for which variable to dig. Default is bathy; also code up draft.
-# For all 3 options, call do_zapping
-# Will have to read in Ua output and possibly reassemble, then write out in binary format for MITgcm
-def adjust_mit_geom (options):
-    pass
+# Given the updated ice shelf draft from Ua, adjust the draft and/or bathymetry so that MITgcm is happy. In order to have fully connected adjacent water columns, they must overlap by at least two wet cells.
+# There are three ways to do this (set in options.digging):
+#    'none': ignore the 2-cell rule and don't dig anything
+#    'bathy': dig bathymetry which is too deep
+#    'draft': dig ice shelf drafts which are too shallow
+# In all cases, also remove ice shelf drafts which are too thin.
+# Ua does not see these changes to the geometry.
 
+# Arguments:
+# ua_draft_file: path to ice shelf draft file written by Ua at the end of the last segment (TODO: define format)
+# bathy_file: path to MITgcm bathymetry file (binary) from the last segment. If options.digging='bathy', it will get overwritten.
+# draft_file: path to desired MITgcm ice shelf draft file (binary).
+# grid: Grid object
+# options: TODO define this
+
+def adjust_mit_geom (ua_draft_file, bathy_file, draft_file, grid, options):
+
+    # TODO: Read Ua draft output and possibly reassemble
+    # Save in variable 'draft'
     
+    # Read MITgcm bathymetry file from last segment
+    bathy = read_binary(bathy_file, [grid.nx, grid.ny], 'xy', prec=options.topo_prec)
 
+    if options.digging == 'none':
+        print 'Not doing digging as per user request'
+    elif options.digging == 'bathy':
+        print 'Digging bathymetry which is too shallow'
+        bathy = do_digging(bathy, draft, grid.dz, grid.z_edges, hFacMin=options.hFacMin, hFacMinDr=options.hFacMinDr, dig_option='bathy')
+    elif options.digging == 'draft':
+        print 'Digging ice shelf drafts which are too deep'
+        draft = do_digging(bathy, draft, grid.dz, grid.z_edges, hFacMin=options.hFacMin, hFacMinDr=options.hFacMinDr, dig_option='draft')
+
+    print 'Zapping ice shelf drafts which are too thin'
+    draft = do_zapping(draft, draft!=0, grid.dz, grid.z_edges, hFacMinDr=options.hFacMinDr)
+    
+    # Ice shelf draft could change in all three cases
+    print 'Writing ' + draft_file
+    write_binary(draft, draft_file, prec=options.topo_prec)
+    if options.digging == 'bathy':
+        # Bathymetry can only change in one case
+        print 'Writing ' + bathy_file
+        write_binary(bathy, bathy_file, prec=options.topo_prec)
+    
 
 # Helper function for set_mit_ics
 # Given 2D fields for bathymetry and ice shelf draft, and information about the vertical grid (which doesn't change over time, so Grid object from last segment is fine), figure out which cells in the 3D grid are (at least partially) open. Return a 3D boolean array.
@@ -70,16 +103,14 @@ def find_open_cells (bathy, draft, grid, options):
     # Identify all z-centres between the two edges. This should be equivalent to ceil(hFacC). TODO: test this equivalence.
     return (z_3d <= edge_above_draft)*(z_3d >= edge_below_bathy)
 
-    
-# TODO
-def set_mit_ics (grid, options):
 
-    # TODO: Read temp, salt, u, v
+
+def set_mit_ics (bathy_file, draft_file, grid, options):
+
+    # TODO: Read temp, salt, u, v. From last pickup (need pickup frequency set correctly, or dump last)? Or diagnostic at the right time?
     if options.use_seaice:
         # TODO: Read aice, hice, hsnow, uice, vice
         pass
-
-    # TODO: Set bathy_file and draft_file
     
     # Read the new ice shelf draft, and also the bathymetry
     print 'Reading ' + draft_file
