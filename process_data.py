0d8948bbb54c986b2183fac48a2552a62bcad502
Kaitlin Naughten <kaight@bas.ac.uk>
2018-11-02 15:24:02 +0000

    Working away on python functions

--- process_data.py
+++ process_data.py
@@ -7,23 +7,28 @@ from scipy.io import savemat
 
 from MITgcmutils import rdmds
 
-from mitgcm_python.utils import convert_ismr, z_to_xyz
+from mitgcm_python.utils import convert_ismr, z_to_xyz, real_dir
 from mitgcm_python.make_domain import model_bdry, level_vars, do_digging, do_zapping
 from mitgcm_python.file_io import read_binary, write_binary
 from mitgcm_python.interpolation import discard_and_fill
+from mitgcm_python.ics_obcs import calc_load_anomaly
 
 
 # Put MITgcm melt rates in the right format for Ua. No need to interpolate.
 
 # Arguments:
-# ismr_head: beginning of file paths of the binary files containing SHIfwFlx data, including the directory. i.e. ismr_head.xxxxx.data and ismr_head.xxxxx.meta where xxxxx is the diagnostic timestep.
+# mit_dir: MITgcm directory containing SHIfwFlx output
 # ua_out_file: desired path to .mat file for Ua to read melt rates from.
 # grid: Grid object (for the MITgcm segment that just finished)
+# options: TODO define this
 
-def extract_melt_rates (ismr_head, ua_out_file, grid):
+def extract_melt_rates (mit_dir, ua_out_file, grid, options):
+
+    # Make sure directory ends in /
+    mit_dir = real_dir(mit_dir)
 
     # Read the most recent file containing ice shelf melt rate
-    data, its, meta = rdmds(ismr_head, itrs=np.Inf, returnmeta=True)
+    data, its, meta = rdmds(mit_dir+options.ismr_head, itrs=np.Inf, returnmeta=True)
     print 'Reading melt rates from ocean timestep ' + str(its)
     # Figure out which index contains SHIfwflx
     i = meta['fldlist'].index('SHIfwFlx')
@@ -50,18 +55,19 @@ def extract_melt_rates (ismr_head, ua_out_file, grid):
 
 # Arguments:
 # ua_draft_file: path to ice shelf draft file written by Ua at the end of the last segment (TODO: define format)
-# bathy_file: path to MITgcm bathymetry file (binary) from the last segment. If options.digging='bathy', it will get overwritten.
-# draft_file: path to desired MITgcm ice shelf draft file (binary).
+# mit_dir: path to MITgcm directory containing binary files for bathymetry and ice shelf draft
 # grid: Grid object
 # options: TODO define this
 
-def adjust_mit_geom (ua_draft_file, bathy_file, draft_file, grid, options):
+def adjust_mit_geom (ua_draft_file, mit_dir, grid, options):
+
+    mit_dir = real_dir(mit_dir)
 
     # TODO: Read Ua draft output and possibly reassemble
     # Save in variable 'draft'
     
     # Read MITgcm bathymetry file from last segment
-    bathy = read_binary(bathy_file, [grid.nx, grid.ny], 'xy', prec=options.topo_prec)
+    bathy = read_binary(mit_dir+options.bathy_file, [grid.nx, grid.ny], 'xy', prec=options.mit_prec)
 
     if options.digging == 'none':
         print 'Not doing digging as per user request'
@@ -76,12 +82,10 @@ def adjust_mit_geom (ua_draft_file, bathy_file, draft_file, grid, options):
     draft = do_zapping(draft, draft!=0, grid.dz, grid.z_edges, hFacMinDr=options.hFacMinDr)
     
     # Ice shelf draft could change in all three cases
-    print 'Writing ' + draft_file
-    write_binary(draft, draft_file, prec=options.topo_prec)
+    write_binary(draft, mit_dir+options.draft_file, prec=options.mit_prec)
     if options.digging == 'bathy':
         # Bathymetry can only change in one case
-        print 'Writing ' + bathy_file
-        write_binary(bathy, bathy_file, prec=options.topo_prec)
+        write_binary(bathy, mit_dir+options.bathy_file, prec=options.mit_prec)
     
 
 # Helper function for set_mit_ics
@@ -105,7 +109,9 @@ def find_open_cells (bathy, draft, grid, options):
 
 
 
-def set_mit_ics (bathy_file, draft_file, grid, options):
+def set_mit_ics (mit_dir, grid, options):
+
+    mit_dir = real_dir(mit_dir)
 
     # TODO: Read temp, salt, u, v. From last pickup (need pickup frequency set correctly, or dump last)? Or diagnostic at the right time?
     if options.use_seaice:
@@ -113,25 +119,39 @@ def set_mit_ics (bathy_file, draft_file, grid, options):
         pass
     
     # Read the new ice shelf draft, and also the bathymetry
-    print 'Reading ' + draft_file
-    draft = read_binary(draft_file, [grid.nx, grid.ny], 'xy', prec=options.topo_prec)
-    print 'Reading ' + bathy_file
-    bathy = read_binary(bathy_file, [grid.nx, grid.ny], 'xy', prec=options.topo_prec)
+    draft = read_binary(mit_dir+options.draft_file, [grid.nx, grid.ny], 'xy', prec=options.mit_prec)
+    bathy = read_binary(mit_dir+options.bathy_file, [grid.nx, grid.ny], 'xy', prec=options.mit_prec)
 
     print 'Selecting newly opened cells'
     # Figure out which cells will be (at least partially) open in the next segment
     open_next = find_open_cells(bathy, draft, grid)
-    # Now select the ones which weren't already open in the last segment
+    # Also save this as a mask with 1s and 0s
+    mask_new = open_next.astype(int)
+    # Now select the open cells which weren't already open in the last segment
     newly_open = open_next*(grid.hfac==0)
 
     print 'Extrapolating temperature into newly opened cells'
     temp_new = discard_and_fill(temp, [], newly_open, missing_val=0)
     print 'Extrapolating salinity into newly opened cells'
     salt_new = discard_and_fill(salt, [], newly_open, missing_val=0)
+    
+    # Write the new initial conditions, masked with 0s (important in case maskIniTemp and/or maskIniSalt are off)
+    write_binary(temp_new*mask_new, mit_dir+options.ini_temp_file, prec=mit_prec)
+    write_binary(salt_new*mask_new, mit_dir+options.ini_salt_file, prec=mit_prec)
 
-    # TODO: Write out all variables including sea ice ones, with new mask applied (inverse of newly_open, select surface for sea ice)
+    # Write the initial conditions which haven't changed
+    # No need to mask them, as velocity and sea ice variables are always masked when they're read in
+    write_binary(u, mit_dir+options.ini_u_file, prec=mit_prec)
+    write_binary(v, mit_dir+options.ini_v_file, prec=mit_prec)
+    if options.use_seaice:
+        write_binary(aice, mit_dir+options.ini_area_file, prec=mit_prec)
+        write_binary(hice, mit_dir+options.ini_heff_file, prec=mit_prec)
+        write_binary(hsnow, mit_dir+options.ini_hsnow_file, prec=mit_prec)
+        write_binary(uice, mit_dir+options.ini_uice_file, prec=mit_prec)
+        write_binary(vice, mit_dir+options.ini_vice_file, prec=mit_prec)
 
-    # TODO: Calculate pload anomaly: write a new function in ics_obcs.py, encapsulate mitgcm_python imports within that module, get calc_load_anomaly to call it with special case of constant T and S. Then write a function here which calls it for all possible options.
+    print 'Calculating pressure load anomaly'
+    calc_load_anomaly(grid, mit_dir+options.pload_file, option=options.pload_option, constant_t=pload_temp, constant_s=pload_salt, ini_temp_file=mit_dir+options.ini_temp_file, ini_salt_file=mit_dir+options.ini_salt_file, eos_type=options.eos_type, rhoConst=options.rhoConst, Talpha=options.Talpha, Sbeta=options.Sbeta, Tref=options.Tref, Sref=options.Sref, prec=options.mit_prec)
 
 
     
