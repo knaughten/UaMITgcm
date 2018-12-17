e39708cab9474f962c4f5e32af9b46b898d6ca47
Jan De Rydt <wchm8@c17777349-lnx.campus.unn.ac.uk>
2018-12-17 14:25:53 +0000

    Read MITgcm tracer grid from XC and YC

--- coupling/ua_development/callUa.m
+++ coupling/ua_development/callUa.m
@@ -2,10 +2,10 @@
 %% TO DO %%
 %%%%%%%%%%%
 %% -> Hardcode appropriate filenames and directories for 
-%   + melt output from MITgcm (MITout_2D.nc?)
+%   + melt output and tracer grid from MITgcm
 %   + calendar file
-%   + user variables
-%% -> Write code for reading user data from file
+%   + file with user variables
+%% -> Write code for reading user variables from file
 %% -> Which netcdf output do we want to generate?
 %% -> What format do I expect for the MITgcm output file: SHIFwFlx in MITout_2D.nc or other?
 
@@ -61,18 +61,23 @@ end
 % 'overstepping'. 
 UserVar.UaMITgcm.ATStimeStepTarget = min(UserVar.UaMITgcm.OutputTimes(2:end)-UserVar.UaMITgcm.OutputTimes(1:end-1));
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Read MITgcm melt rates %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MITgcm is kg w.e./s, convert to m/yr 
+% negative for melting
+MeltFile = [UserVar.UaMITgcm.MITgcmOutputsDirectory,'/MITout_2D.nc'];
+UserVar.UaMITgcm.MITgcmMelt = double(ncread(MeltFile,'SHIfwFlx')/1000*365*24*60*60);
+UserVar.UaMITgcm.MITgcmMelt = squeeze(UserVar.UaMITgcm.MITgcmMelt(:,:,end));
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Read MITgcm grid and check if it’s lat/lon or Cartesian %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% read grid from MITgcm melt output
-str2D=strcat(UserVar.UaMITgcm.MITgcmOutputsDirectory,'/MITout_2D.nc');
-lon = double(ncread(str2D,'LONGITUDE')); Ilon = size(lon);
-lat = double(ncread(str2D,'LATITUDE'));
-
-% make sure grid is 2D field
-if Ilon(1)==1 || Ilon(2)==1
-   [lon,lat] = ndgrid(lon(:),lat(:));
-end
+% read tracer gridpoints
+fid=fopen([UserVar.UaMITgcm.MITgcmOutputsDirectory,'/XC.data'],'r','b');
+lon=fread(fid,inf,'real*4');    Ilon = size(lon); fclose(fid);
+fid=fopen([UserVar.UaMITgcm.MITgcmOutputsDirectory,'/YC.data'],'r','b');
+lat=fread(fid,inf,'real*4'); fclose(fid);
 
 % check if grid is lat/lon and convert to cartesian if required
 if all(lon(:)>=-180) && all(lon(:)<=180) && all(lat(:)>=-90) && all(lat(:)<=90)
@@ -81,18 +86,11 @@ else
     x = lon;    y = lat;
 end
     
+% reshape to 2D grid
+[I,J] = size(UserVar.UaMITgcm.MITgcmMelt);
+x = reshape(x,I,J); y = reshape(y,I,J);
 UserVar.UaMITgcm.MITgcmGridX = x;
 UserVar.UaMITgcm.MITgcmGridY = y;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Read MITgcm meltrates %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[I,J] = size(x);
-time = ncread(str2D,'TIME'); T=length(time);
-
-% MITgcm is kg w.e./s, convert to m/yr 
-% negative for melting
-UserVar.UaMITgcm.MITgcmMelt = double(ncread(str2D,'SHIfwFlx',[1,1,T],[I,J,1])/1000*365*24*60*60);
 
 %%%%%%%%%%%%
 %% run Ua %%
