4b47ee1e7b6211fa57fb459bddb4b6b22dc685a3
Kaitlin Naughten <kaight@bas.ac.uk>
2019-01-25 13:16:13 +0000

    Script to clean the run

--- example/MISOMIP/MISOMIP_999/scripts/prepare_run.sh
+++ example/MISOMIP/MISOMIP_999/scripts/prepare_run.sh
@@ -3,6 +3,19 @@
 # Clean out old results and link input files.
 ################################################
 
+# Save current directory
+ORIG_DIR=$PWD
+
+# Optional argument: path to scripts/ directory.
+# Can be useful if this script was called from a different directory.
+# If unset, assume we're already in it
+SCRIPT_DIR=$1
+if [ -z "$SCRIPT_DIR" ]; then
+    SCRIPT_DIR=ORIG_DIR
+fi
+
+cd $SCRIPT_DIR
+
 # Empty the run directory - but first make sure it exists!
 if [ -d "../run" ]; then
   cd ../run
@@ -18,3 +31,5 @@ cp ../input/* .
 
 # Link executable
 ln -s ../build/mitgcmuv .
+
+cd $ORIG_DIR
