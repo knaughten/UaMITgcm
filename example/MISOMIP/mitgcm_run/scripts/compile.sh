3732703e753883eeebb8bf2c7f74bb80bf27c790
Kaitlin Naughten <kaight@bas.ac.uk>
2019-01-28 14:52:41 +0000

    Put build_options inside mitgcm_run

--- example/MISOMIP/mitgcm_run/scripts/compile.sh
+++ example/MISOMIP/mitgcm_run/scripts/compile.sh
@@ -7,7 +7,7 @@
 # Path to MITgcm source code: recommend to use the one inside UaMITgcm
 MIT_SOURCE=$WORK/UaMITgcm/MITgcm
 # Path to file containing Fortran flags etc
-BUILD_OPTIONS=$WORK/mitgcm/build_options/linux_amd64_archer_ifort
+BUILD_OPTIONS=../linux_amd64_archer_ifort
 
 # Empty the build directory - but first make sure it exists!
 if [ -d "../build" ]; then
