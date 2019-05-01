5d53802cc8c103f433b051dad05ca1cf121a56be
Kaitlin Naughten <kaight@bas.ac.uk>
2019-05-01 14:02:47 +0100

    Wrap python scripts in main-check so they can import each other if needed

--- coupling/clean.py
+++ coupling/clean.py
@@ -8,55 +8,64 @@ import subprocess
 
 from set_parameters import Options
 
-# Make sure the user didn't call this accidentally
-out = raw_input('This will delete all existing results if they are not backed up. Are you sure you want to proceed (yes/no)? ').strip()
-while True:
-    if out == 'yes':
-        break
-    if out == 'no':
-        sys.exit()
-    out = raw_input('Please answer yes or no. ').strip()
+# Function to clean the given Ua executable directory
+# This is also called by restart.py in some circumstances, so encapsulate it.
+def clean_ua (directory):
+    # Look at everything in the Ua executable directory
+    for fname in os.listdir(directory):
+        if fname.endswith('RestartFile.mat'):
+            # Save the name of the restart file
+            restart_name = fname
+        # Don't care about mesh files
+        elif fname in ['NewMeshFile.mat', 'AdaptMesh.mat']:
+            pass    
+        # Don't delete the Ua executable, run script, or other .mat files
+        elif fname in ['Ua', 'Ua_MCR.sh'] or fname.endswith('.mat'):
+            continue
+        # Delete everything else
+        path = directory+fname
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+    # Copy in the original restart with the correct name
+    orig_restart = raw_input('Enter path to original Ua restart file, or press enter if this is not a restart run: ')
+    if len(orig_restart) > 0:
+        while True:
+            if os.path.isfile(orig_restart):
+                break
+            orig_restart = raw_input('That file does not exist. Try again: ')
+        shutil.copyfile(orig_restart, directory+restart_name)
 
-# Read simulation options so we have directories
-options = Options()
 
-# Remove everything in the output directory
-# Easiest just to delete the whole directory and then create it again
-shutil.rmtree(options.output_dir)
-os.mkdir(options.output_dir)
+# Main processing
+if __name__ == "__main__":
 
-# Look at everything in the Ua executable directory
-for fname in os.listdir(options.ua_exe_dir):
-    if fname.endswith('RestartFile.mat'):
-        # Save the name of the restart file
-        restart_name = fname
-    # Don't care about mesh files
-    elif fname in ['NewMeshFile.mat', 'AdaptMesh.mat']:
-        pass    
-    # Don't delete the Ua executable, run script, or other .mat files
-    elif fname in ['Ua', 'Ua_MCR.sh'] or fname.endswith('.mat'):
-        continue
-    # Delete everything else
-    path = options.ua_exe_dir+fname
-    if os.path.isfile(path):
-        os.remove(path)
-    elif os.path.isdir(path):
-        shutil.rmtree(path)
-
-# Copy in the original restart with the correct name
-orig_restart = raw_input('Enter path to original Ua restart file, or press enter if this is not a restart run: ')
-if len(orig_restart) > 0:
+    # Make sure the user didn't call this accidentally
+    out = raw_input('This will delete all existing results if they are not backed up. Are you sure you want to proceed (yes/no)? ').strip()
     while True:
-        if os.path.isfile(orig_restart):
+        if out == 'yes':
             break
-        orig_restart = raw_input('That file does not exist. Try again: ')
-    shutil.copyfile(orig_restart, options.ua_exe_dir+restart_name)
+        if out == 'no':
+            sys.exit()
+        out = raw_input('Please answer yes or no. ').strip()
 
-# Now call the prepare_run.sh script to reset the MITgcm run directory
-# Pass the path to scripts/ as an argument, because prepare_run.sh is called from outside its own directory
-subprocess.check_output([options.mit_case_dir+'scripts/prepare_run.sh', options.mit_case_dir+'scripts/'])
+    # Read simulation options so we have directories
+    options = Options()
 
-# Delete some log files if they exist
-for fname in os.listdir('./'):
-    if fname in ['coupler_stdout', 'jobs.log'] or (fname.startswith('run_') and '.sh.o' in fname):
-        os.remove(fname)
+    # Remove everything in the output directory
+    # Easiest just to delete the whole directory and then create it again
+    shutil.rmtree(options.output_dir)
+    os.mkdir(options.output_dir)
+
+    # Clean Ua executable directory
+    clean_ua(options.ua_exe_dir)
+
+    # Now call the prepare_run.sh script to reset the MITgcm run directory
+    # Pass the path to scripts/ as an argument, because prepare_run.sh is called from outside its own directory
+    subprocess.check_output([options.mit_case_dir+'scripts/prepare_run.sh', options.mit_case_dir+'scripts/'])
+
+    # Delete some log files if they exist
+    for fname in os.listdir('./'):
+        if fname in ['coupler_stdout', 'jobs.log'] or (fname.startswith('run_') and '.sh.o' in fname):
+            os.remove(fname)
--- coupling/master.py
+++ coupling/master.py
@@ -3,76 +3,78 @@ from mitgcm_python.grid import Grid
 from process_data import zero_ini_files, copy_grid, extract_melt_rates, adjust_mit_geom, adjust_mit_state, convert_mit_output, gather_output
 from coupling_utils import submit_job
 
+# Top-level coupling function.
+if __name__ == "__main__":
 
-print 'Reading parameters'
-options = Options()
+    print 'Reading parameters'
+    options = Options()
 
-print 'Checking calendar'
-initial, restart, spinup, first_coupled, finished = set_calendar(options.output_dir, options.mit_run_dir, options)
+    print 'Checking calendar'
+    initial, restart, spinup, first_coupled, finished = set_calendar(options.output_dir, options.mit_run_dir, options)
 
-if initial and options.restart_type=='zero':
-    print 'Creating dummy initial conditions files where needed'
-    zero_ini_files(options)
-    
-
-# Do we need to do any processing for the next run?
-if not initial and not restart:
-    
-    print 'Building MITgcm grid'
-    grid = Grid(options.mit_run_dir)
-
-    # Do we need to do any coupling?
-    if not spinup:
-
-        print 'Sending MITgcm grid to Ua'
-        copy_grid(options.mit_run_dir, options.output_dir)
-
-        print 'Sending melt rates from MITgcm to Ua'
-        extract_melt_rates(options.mit_run_dir, options.output_dir+options.ua_melt_file, grid, options)
-
-        # Do we need to change the ice shelf draft in MITgcm?
-        if not first_coupled:
-
-            print 'Adjusting MITgcm topography'
-            adjust_mit_geom(options.ua_output_dir+options.ua_draft_file, options.mit_run_dir, grid, options)
-
-    if options.restart_type=='zero':
-        print 'Setting new initial conditions for MITgcm'        
-    elif options.restart_type=='pickup':        
-        print 'Adjusting MITgcm pickup file'
-    adjust_mit_state(options.mit_run_dir, grid, options)
-
-    
-# Is there any output we need to deal with?
-if not initial and not restart:
-    
-    if options.use_xmitgcm:
-        print 'Converting MITgcm binary output to NetCDF'
-        convert_mit_output(options)
-
-    print 'Gathering output'
-    gather_output(options, spinup, first_coupled)
+    if initial and options.restart_type=='zero':
+        print 'Creating dummy initial conditions files where needed'
+        zero_ini_files(options)
 
 
-# Do we need to submit more jobs?
-if not finished:
+    # Do we need to do any processing for the next run?
+    if not initial and not restart:
 
-    print 'Submitting next MITgcm segment'
-    mit_id = submit_job(options, 'run_mitgcm.sh', input_var=['MIT_DIR='+options.mit_case_dir])
-    afterok = [mit_id]
+        print 'Building MITgcm grid'
+        grid = Grid(options.mit_run_dir)
 
-    # Do we also need to submit Ua job?
-    if not spinup:
-        print 'Submitting next Ua segment'
-        if options.ua_option == 'compiled':
-            ua_id = submit_job(options, 'run_ua.sh', input_var=['UA_DIR='+options.ua_exe_dir])
-        elif options.ua_option == 'matlab':
-            # TODO
-            pass
-        afterok.append(ua_id)
+        # Do we need to do any coupling?
+        if not spinup:
 
-    print 'Submitting next coupler job to start after segment is finished'
-    submit_job(options, 'run_coupler.sh', afterok=afterok)
-    
+            print 'Sending MITgcm grid to Ua'
+            copy_grid(options.mit_run_dir, options.output_dir)
 
-print 'Coupling successfully completed'
+            print 'Sending melt rates from MITgcm to Ua'
+            extract_melt_rates(options.mit_run_dir, options.output_dir+options.ua_melt_file, grid, options)
+
+            # Do we need to change the ice shelf draft in MITgcm?
+            if not first_coupled:
+
+                print 'Adjusting MITgcm topography'
+                adjust_mit_geom(options.ua_output_dir+options.ua_draft_file, options.mit_run_dir, grid, options)
+
+        if options.restart_type=='zero':
+            print 'Setting new initial conditions for MITgcm'        
+        elif options.restart_type=='pickup':        
+            print 'Adjusting MITgcm pickup file'
+        adjust_mit_state(options.mit_run_dir, grid, options)
+
+
+    # Is there any output we need to deal with?
+    if not initial and not restart:
+
+        if options.use_xmitgcm:
+            print 'Converting MITgcm binary output to NetCDF'
+            convert_mit_output(options)
+
+        print 'Gathering output'
+        gather_output(options, spinup, first_coupled)
+
+
+    # Do we need to submit more jobs?
+    if not finished:
+
+        print 'Submitting next MITgcm segment'
+        mit_id = submit_job(options, 'run_mitgcm.sh', input_var=['MIT_DIR='+options.mit_case_dir])
+        afterok = [mit_id]
+
+        # Do we also need to submit Ua job?
+        if not spinup:
+            print 'Submitting next Ua segment'
+            if options.ua_option == 'compiled':
+                ua_id = submit_job(options, 'run_ua.sh', input_var=['UA_DIR='+options.ua_exe_dir])
+            elif options.ua_option == 'matlab':
+                # TODO
+                pass
+            afterok.append(ua_id)
+
+        print 'Submitting next coupler job to start after segment is finished'
+        submit_job(options, 'run_coupler.sh', afterok=afterok)
+
+
+    print 'Coupling successfully completed'
