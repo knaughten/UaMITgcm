96a45664b633f3dbea2f25e52645da0bc96a58c5
Kaitlin Naughten <Kaitlin Naughten>
2022-06-10 13:34:13 +0100

    Bug fixes

--- coupling/reduce_output.py
+++ coupling/reduce_output.py
@@ -4,11 +4,10 @@
 import sys
 import os
 
-from set_parameters import Options
-
 years_skip = []
-for i in len(sys.argv):
-    years_skip.append(int(sys.argv[i]))
+if len(sys.argv) > 1:
+    for i in range(1,len(sys.argv)):
+        years_skip.append(int(sys.argv[i]))
 
 message = 'Output will be reduced for all years'
 if len(years_skip) > 0:
@@ -23,13 +22,13 @@ while True:
         sys.exit()
     out = input('Please answer yes or no. ').strip()
 
-options = Options()
-for dname in os.listdir(options.output_dir):
-    if os.path.isdir(options.output_dir+dname):
+output_dir = 'output/'
+for dname in os.listdir(output_dir):
+    if os.path.isdir(output_dir+dname):
         if int(dname[:4]) in years_skip:
             print('Skipping '+dname[:4])
             continue
-        date_dir = options.output_dir+dname+'/MITgcm/'
+        date_dir = output_dir+dname+'/MITgcm/'
         for fname in os.listdir(date_dir):
             if fname.endswith('.nc'):
                 continue
